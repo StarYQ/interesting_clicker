e82a3662a6c38dd59b32aab649d09d06101fa864
Arnab Bhowmik <46697160+StarYQ@users.noreply.github.com>
2025-02-20 17:21:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+(function() {
+  var totalDuration = 80 * 1000;
+  var videoClickDuration = 1000; 
+  var clickInterval = 1; 
+  var buttonClickCount = 20; 
+
+  var automationStart = Date.now();
+
+  var video = document.querySelector("video");
+  if (!video) {
+    console.log("No video element found.");
+    return;
+  }
+
+  var buyButton = document.querySelector(".buyButton");
+  if (!buyButton) {
+    console.log("No buy button found.");
+    return;
+  }
+
+  function runCycle() {
+    if (Date.now() - automationStart >= totalDuration) {
+      console.log("Automation stopped after " + totalDuration / 1000 + " seconds.");
+      return;
+    }
+
+the video for 1 second.
+    var cycleStart = Date.now();
+    var videoInterval = setInterval(function() {
+      video.click();
+      if (Date.now() - cycleStart >= videoClickDuration) {
+        clearInterval(videoInterval);
+
+click the buy button 20 times.
+        var clicks = 0;
+        var buttonInterval = setInterval(function() {
+          buyButton.click();
+          clicks++;
+          if (clicks >= buttonClickCount) {
+            clearInterval(buttonInterval);
+immediately after finishing button clicks.
+            runCycle();
+          }
+        }, clickInterval);
+      }
+    }, clickInterval);
+  }
+
+  console.log("Starting automation for " + totalDuration / 1000 + " seconds...");
+  runCycle();
+})();
